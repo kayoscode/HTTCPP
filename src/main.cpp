277bb7571a13600ce084df2f223bb7d4fb0aa74b
Bryce <bryce@ubuntu.bryce>
2021-04-12 17:46:02 -0600

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
 #include "AESEngine.h"
 
 int main() {
-    unsigned char keyData[16] { 0x0f, 0x00, 0x71, 0xc9, 0x47, 0xd9, 0xe8, 0x59, 0x1c, 0xb7, 0xad, 0xd6, 0xaf, 0x7f, 0x67, 0x98 };
+    unsigned char keyData[] = "0000000000000001";
     AESKey* key = new AESKey(keyData, AESKeyType::AES_KEY128);
     AESEngine aes(key);
 
     std::string output;
-    aes.encyrptText("0321456789abcdeffedcba9876543210", output);
+    aes.encyrptText("bryceyoungreallyisthebestpersonintheworld", output);
 
     return 0;
 }
